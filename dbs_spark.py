e7f5a853b24cca7806d15c1f4fedc31a6beef30f
Valentin <vkuznet@gmail.com>
2016-11-18 14:57:32 +0100

    Removed obsolete code

--- dbs_spark.py
+++ dbs_spark.py
@@ -16,13 +16,8 @@ https://spark.apache.org/docs/0.9.0/api/pyspark/index.html
 # system modules
 import os
 import sys
-import imp
-import pwd
 import time
 import json
-import urllib
-import urllib2
-import httplib
 import argparse
 
 from pyspark import SparkContext
@@ -30,10 +25,7 @@ from pyspark.sql import Row
 from pyspark.sql import SQLContext
 from pyspark.sql import HiveContext
 from pyspark.sql import DataFrame
-from pyspark.sql.window import Window
 from pyspark.sql.types import DoubleType, IntegerType, StructType, StructField, StringType, BooleanType, LongType
-from pyspark.sql import functions as F
-from pyspark.sql.functions import udf, from_unixtime, date_format, regexp_extract, when, lit, lag, lead, coalesce, sum, rowNumber
 
 class OptionParser():
     def __init__(self):
@@ -59,60 +51,6 @@ class OptionParser():
             dest="yarn", default=False, help="run job on analytics cluster via yarn resource manager")
         self.parser.add_argument("--verbose", action="store_true",
             dest="verbose", default=False, help="verbose output")
-
-def x509():
-    "Helper function to get x509 either from env or tmp file"
-    proxy = os.environ.get('X509_USER_PROXY', '')
-    if  not proxy:
-        proxy = '/tmp/x509up_u%s' % pwd.getpwuid( os.getuid() ).pw_uid
-        if  not os.path.isfile(proxy):
-            return ''
-    return proxy
-
-class HTTPSClientAuthHandler(urllib2.HTTPSHandler):
-    """
-    Simple HTTPS client authentication class based on provided
-    key/ca information
-    """
-    def __init__(self, key=None, cert=None, level=0):
-        if  level > 1:
-            urllib2.HTTPSHandler.__init__(self, debuglevel=1)
-        else:
-            urllib2.HTTPSHandler.__init__(self)
-        self.key = key
-        self.cert = cert
-
-    def https_open(self, req):
-        """Open request method"""
-        #Rather than pass in a reference to a connection class, we pass in
-        # a reference to a function which, for all intents and purposes,
-        # will behave as a constructor
-        return self.do_open(self.get_connection, req)
-
-    def get_connection(self, host, timeout=300):
-        """Connection method"""
-        if  self.key:
-            return httplib.HTTPSConnection(host, key_file=self.key,
-                                                cert_file=self.cert)
-        return httplib.HTTPSConnection(host)
-
-def postdata(url, data, ckey=None, cert=None, verbose=0):
-    """
-    POST data into given url
-    """
-    headers = {'Content-type':'application/json','Accept':'application/json'}
-    req = urllib2.Request(url)
-    for key, val in headers.iteritems():
-        req.add_header(key, val)
-    if  verbose > 1:
-        handler = urllib2.HTTPHandler(debuglevel=1)
-        opener  = urllib2.build_opener(handler)
-        urllib2.install_opener(opener)
-    if  ckey and cert:
-        handler = HTTPSClientAuthHandler(ckey, cert, verbose)
-        opener  = urllib2.build_opener(handler)
-        urllib2.install_opener(opener)
-    data = urllib2.urlopen(req, json.dumps(data))
 
 class SparkLogger(object):
     "Control Spark Logger"
