80b57872760d55e7b68ecba277a21abcd41d5d35
Valentin <vkuznet@gmail.com>
2017-04-12 20:14:50 +0200

    Move general utilities into utils

--- dbs_phedex.py
+++ dbs_phedex.py
@@ -11,7 +11,6 @@ Description: Spark script to parse DBS and PhEDEx content on HDFS
 import os
 import re
 import sys
-import gzip
 import time
 import json
 import argparse
@@ -22,6 +21,7 @@ from pyspark.sql import HiveContext
 
 # local modules
 from spark_utils import dbs_tables, phedex_tables, print_rows, spark_context
+from utils import elapsed_time
 
 # WMCore modules
 try:
@@ -66,66 +66,6 @@ class OptionParser():
         msg = "Send results via StompAMQ to a broker, provide broker credentials in JSON file"
         self.parser.add_argument("--amq", action="store",
             dest="amq", default="", help=msg)
-
-class GzipFile(gzip.GzipFile):
-    def __enter__(self):
-        "Context manager enter method"
-        if self.fileobj is None:
-            raise ValueError("I/O operation on closed GzipFile object")
-        return self
-
-    def __exit__(self, *args):
-        "Context manager exit method"
-        self.close()
-
-def fopen(fin, mode='r'):
-    "Return file descriptor for given file"
-    if  fin.endswith('.gz'):
-        stream = gzip.open(fin, mode)
-        # if we use old python we switch to custom gzip class to support
-        # context manager and with statements
-        if  not hasattr(stream, "__exit__"):
-            stream = GzipFile(fin, mode)
-    else:
-        stream = open(fin, mode)
-    return stream
-
-def htime(seconds):
-    "Convert given seconds into human readable form of N hour(s), N minute(s), N second(s)"
-    minutes, secs = divmod(seconds, 60)
-    hours, minutes = divmod(minutes, 60)
-    days, hours = divmod(hours, 24)
-    def htimeformat(msg, key, val):
-        "Helper function to proper format given message/key/val"
-        if  val:
-            if  msg:
-                msg += ', '
-            msg += '%d %s' % (val, key)
-            if  val > 1:
-                msg += 's'
-        return msg
-
-    out = ''
-    out = htimeformat(out, 'day', days)
-    out = htimeformat(out, 'hour', hours)
-    out = htimeformat(out, 'minute', minutes)
-    out = htimeformat(out, 'second', secs)
-    return out
-
-def elapsed_time(time0):
-    "Return elapsed time from given time stamp"
-    return htime(time.time()-time0)
-
-def unix_tstamp(date):
-    "Convert given date into unix seconds since epoch"
-    if  not isinstance(date, str):
-        raise NotImplementedError('Given date %s is not in string YYYYMMDD format' % date)
-    if  len(date) == 8:
-        return int(time.mktime(time.strptime(date, '%Y%m%d')))
-    elif len(date) == 10: # seconds since epoch
-        return int(date)
-    else:
-        raise NotImplementedError('Given date %s is not in string YYYYMMDD format' % date)
 
 def run(fout, verbose=None, yarn=None, tier=None, era=None,
         release=None, cdate=None, patterns=[], antipatterns=[]):
---
+++ utils.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+#pylint: disable=
+"""
+File       : utils.py
+Author     : Valentin Kuznetsov <vkuznet AT gmail dot com>
+Description: Set of utililities
+"""
+
+# system modules
+import gzip
+import time
+
+class GzipFile(gzip.GzipFile):
+    def __enter__(self):
+        "Context manager enter method"
+        if self.fileobj is None:
+            raise ValueError("I/O operation on closed GzipFile object")
+        return self
+
+    def __exit__(self, *args):
+        "Context manager exit method"
+        self.close()
+
+def fopen(fin, mode='r'):
+    "Return file descriptor for given file"
+    if  fin.endswith('.gz'):
+        stream = gzip.open(fin, mode)
+        # if we use old python we switch to custom gzip class to support
+        # context manager and with statements
+        if  not hasattr(stream, "__exit__"):
+            stream = GzipFile(fin, mode)
+    else:
+        stream = open(fin, mode)
+    return stream
+
+def htime(seconds):
+    "Convert given seconds into human readable form of N hour(s), N minute(s), N second(s)"
+    minutes, secs = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    def htimeformat(msg, key, val):
+        "Helper function to proper format given message/key/val"
+        if  val:
+            if  msg:
+                msg += ', '
+            msg += '%d %s' % (val, key)
+            if  val > 1:
+                msg += 's'
+        return msg
+
+    out = ''
+    out = htimeformat(out, 'day', days)
+    out = htimeformat(out, 'hour', hours)
+    out = htimeformat(out, 'minute', minutes)
+    out = htimeformat(out, 'second', secs)
+    return out
+
+def elapsed_time(time0):
+    "Return elapsed time from given time stamp"
+    return htime(time.time()-time0)
+
+def unix_tstamp(date):
+    "Convert given date into unix seconds since epoch"
+    if  not isinstance(date, str):
+        raise NotImplementedError('Given date %s is not in string YYYYMMDD format' % date)
+    if  len(date) == 8:
+        return int(time.mktime(time.strptime(date, '%Y%m%d')))
+    elif len(date) == 10: # seconds since epoch
+        return int(date)
+    else:
+        raise NotImplementedError('Given date %s is not in string YYYYMMDD format' % date)
+
+
