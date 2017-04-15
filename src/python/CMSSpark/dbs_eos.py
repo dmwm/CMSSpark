19018f116522238dd0efa2b09563e059bd078067
Valentin <vkuznet@gmail.com>
2017-04-15 16:24:01 +0200

    New scripts for aaa/eos

---
+++ src/python/CMSSpark/dbs_eos.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+#pylint: disable=
+"""
+File       : dbs_eos.py
+Author     : Valentin Kuznetsov <vkuznet AT gmail dot com>
+Description: Spark script to parse DBS and Xrootd monitoring content on HDFS
+"""
+
+# system modules
+import os
+import re
+import sys
+import gzip
+import time
+import json
+import argparse
+from types import NoneType
+
+from pyspark import SparkContext, StorageLevel
+from pyspark.sql import HiveContext
+from pyspark.sql.functions import lit
+
+# CMSSpark modules
+from CMSSpark.spark_utils import dbs_tables, phedex_tables, print_rows, spark_context, eos_tables
+from CMSSpark.utils import elapsed_time, cern_monit
+
+class OptionParser():
+    def __init__(self):
+        "User based option parser"
+        self.parser = argparse.ArgumentParser(prog='PROG')
+        year = time.strftime("%Y", time.localtime())
+        hdir = 'hdfs:///project/awg/cms'
+        msg = 'Location of CMS folders on HDFS, default %s' % hdir
+        self.parser.add_argument("--hdir", action="store",
+            dest="hdir", default=hdir, help=msg)
+        fout = 'eos_datasets.csv'
+        self.parser.add_argument("--fout", action="store",
+            dest="fout", default=fout, help='Output file name, default %s' % fout)
+        self.parser.add_argument("--date", action="store",
+            dest="date", default="", help='Select CMSSW data for specific date (YYYYMMDD)')
+        self.parser.add_argument("--no-log4j", action="store_true",
+            dest="no-log4j", default=False, help="Disable spark log4j messages")
+        self.parser.add_argument("--yarn", action="store_true",
+            dest="yarn", default=False, help="run job on analytics cluster via yarn resource manager")
+        self.parser.add_argument("--verbose", action="store_true",
+            dest="verbose", default=False, help="verbose output")
+        msg = "Send results via StompAMQ to a broker, provide broker credentials in JSON file"
+        self.parser.add_argument("--amq", action="store",
+            dest="amq", default="", help=msg)
+
+def eos_date(date):
+    "Convert given date into eos date format"
+    if  not date:
+        date = time.strftime("%Y/%m/%d", time.gmtime(time.time()-60*60*24))
+        return date
+    if  len(date) != 8:
+        raise Exception("Given date %s is not in YYYYMMDD format")
+    year = date[:4]
+    month = date[4:6]
+    day = date[6:]
+    return '%s/%s/%s' % (year, month, day)
+
+def eos_date_unix(date):
+    "Convert EOS date into UNIX timestamp"
+    return time.mktime(time.strptime(date, '%Y/%m/%d'))
+
+def run(fout, verbose=None, yarn=None, date=None):
+    """
+    Main function to run pyspark job. It requires a schema file, an HDFS directory
+    with data and optional script with mapper/reducer functions.
+    """
+    time0 = time.time()
+
+    # define spark context, it's main object which allow to communicate with spark
+    ctx = spark_context('cms', yarn, verbose)
+    sqlContext = HiveContext(ctx)
+
+    # read DBS and Phedex tables
+    tables = {}
+    tables.update(dbs_tables(sqlContext, verbose=verbose))
+    ddf = tables['ddf'] # dataset table
+    fdf = tables['fdf'] # file table
+
+    if  verbose:
+        for row in ddf.head(1):
+            print("### ddf row", row)
+
+    # read CMSSW avro rdd
+    date = eos_date(date)
+    tables.update(eos_tables(sqlContext, date=date, verbose=verbose))
+    eos_df = tables['eos_df'] # EOS table
+
+    if  verbose:
+        for row in eos_df.head(1):
+            print("### eos_df row", row)
+
+    # merge DBS and CMSSW data
+    cols = ['d_dataset','d_dataset_id','f_logical_file_name','file_lfn']
+    stmt = 'SELECT %s FROM ddf JOIN fdf ON ddf.d_dataset_id = fdf.f_dataset_id JOIN eos_df ON fdf.f_logical_file_name = eos_df.file_lfn' % ','.join(cols)
+    joins = sqlContext.sql(stmt)
+    print_rows(joins, stmt, verbose)
+
+    # perform aggregation
+    fjoin = joins.groupBy(['d_dataset'])\
+            .agg({'file_lfn':'count'})\
+            .withColumnRenamed('count(file_lfn)', 'count')\
+            .withColumnRenamed('d_dataset', 'dataset')\
+            .withColumn('date', lit(aaa_date_unix(date)))\
+            .withColumn('count_type', lit('eos'))\
+
+    # keep table around
+    fjoin.persist(StorageLevel.MEMORY_AND_DISK)
+
+    # write out results back to HDFS, the fout parameter defines area on HDFS
+    # it is either absolute path or area under /user/USERNAME
+    if  fout:
+        fjoin.write.format("com.databricks.spark.csv")\
+                .option("header", "true").save(fout)
+
+    ctx.stop()
+    if  verbose:
+        print("Elapsed time %s" % elapsed_time(time0))
+    return fjoin
+
+def tmp():
+    # output results
+    out = []
+    idx = 0
+    for row in fjoin.collect():
+        rdict = row.asDict()
+        rdict['date'] = date
+        out.append(rdict)
+        if  verbose and idx < 5:
+            print(rdict)
+        idx += 1
+
+    # write out output
+    if  fout and out:
+        with open(fout, 'w') as ostream:
+            headers = sorted(out[0].keys())
+            ostream.write(','.join(headers)+'\n')
+            for rdict in out:
+                arr = []
+                for key in headers:
+                    arr.append(str(rdict[key]))
+                ostream.write(','.join(arr)+'\n')
+
+    ctx.stop()
+    if  verbose:
+        print("Elapsed time %s" % elapsed_time(time0))
+    return out
+
+def main():
+    "Main function"
+    optmgr  = OptionParser()
+    opts = optmgr.parser.parse_args()
+    print("Input arguments: %s" % opts)
+    time0 = time.time()
+    fout = opts.fout
+    verbose = opts.verbose
+    yarn = opts.yarn
+    date = opts.date
+    res = run(fout, verbose, yarn, date)
+    if  opts.amq:
+        cern_monit(res)
+
+    print('Start time  : %s' % time.strftime('%Y-%m-%d %H:%M:%S GMT', time.gmtime(time0)))
+    print('End time    : %s' % time.strftime('%Y-%m-%d %H:%M:%S GMT', time.gmtime(time.time())))
+    print('Elapsed time: %s sec' % elapsed_time(time0))
+
+if __name__ == '__main__':
+    main()
