f1bcfc5073eb60bd8f44573567a2058d942eee82
Valentin <vkuznet@gmail.com>
2017-12-07 13:22:10 +0100

    Add start date

--- src/python/CMSSpark/dates.py
+++ src/python/CMSSpark/dates.py
@@ -17,6 +17,8 @@ class OptionParser():
     def __init__(self):
         "User based option parser"
         self.parser = argparse.ArgumentParser(prog='PROG')
+        self.parser.add_argument("--start", action="store",
+            dest="start", default="", help="start date (YYYYMMDD)")
         self.parser.add_argument("--ndays", action="store",
             dest="ndays", default=30, help="Number of days, default 30")
         iformat = '%Y-%m-%d'
@@ -25,13 +27,23 @@ class OptionParser():
         self.parser.add_argument("--range", action="store_true",
             dest="range", default=False, help="show full range")
 
-def dates(numdays):
+def dates(start, numdays):
     base = datetime.datetime.today()
+    if start:
+        year = int(start[:4])
+        month = int(start[4:6])
+        day = int(start[6:])
+        base = datetime.datetime(year, month, day)
     date_list = [base - datetime.timedelta(days=x) for x in range(0, numdays)]
     return date_list
 
-def range_dates(numdays):
+def range_dates(start, numdays):
     base = datetime.datetime.today()
+    if start:
+        year = int(start[:4])
+        month = int(start[4:6])
+        day = int(start[6:])
+        base = datetime.datetime(year, month, day)
     for step in range(0, numdays):
         yield base-datetime.timedelta(days=step)
 
@@ -43,10 +55,10 @@ def main():
     optmgr  = OptionParser()
     opts = optmgr.parser.parse_args()
     if opts.range:
-        for date in range_dates(int(opts.ndays)):
+        for date in range_dates(opts.start, int(opts.ndays)):
             print(dformat(date, opts.format))
     else:
-        date_list = dates(int(opts.ndays))
+        date_list = dates(opts.start, int(opts.ndays))
         min_date, max_date = date_list[-1], date_list[0]
         print('%s %s' % (dformat(min_date, opts.format), dformat(max_date, opts.format)))
 
