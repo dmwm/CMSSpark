0fdc1022b9ea9311037d9f34273baa62911b8d22
David <David.Lange@cern.ch>
2017-12-27 14:17:27 +0100

    first version of entry merging/cleaning script - it just looks for gid=-1 next to a real gid in time and then combines the records

--- src/python/CMSSpark/mergePhedex.py
+++ src/python/CMSSpark/mergePhedex.py
@@ -17,6 +17,7 @@ import sys
 import time
 import argparse
 import subprocess
+import datetime
 
 class OptionParser():
     def __init__(self):
@@ -52,6 +53,7 @@ def update(rdict, line):
     rdate = int(rdate)
     if gid == "null": 
         gid="-1"
+    gid=int(gid)
     key = (site, dataset, rdate, gid)
     if key in rdict:
         _min_date, _max_date, _ave_size, _last_size, _nom_days = rdict[key]
@@ -70,6 +72,85 @@ def update(rdict, line):
     else:
         days = 1
         rdict[key] = [date, date, size, size, days]
+    return
+
+def clean(rdict):
+    rdict3={}
+    for key,val in rdict.iteritems():
+        t=list(key)[0:3] #all but gid
+        key3=(t[0],t[1],t[2])
+        if key3 not in rdict3:
+            rdict3[key3]=[]
+        rdict3[key3].append(list(key)+list(val))
+
+    minDateCol=0
+    maxDateCol=1
+    maxSizeCol=2
+    daysCol=4
+    aveSizeCol=3
+
+    key3s=rdict3.keys()
+    for k in key3s:
+        foundIt=True
+        while foundIt:
+            foundIt=False
+            vals=rdict3[k]
+            n=len(vals)
+            if n==1: continue
+            for i in range(n):
+                if foundIt:break
+                gid1=vals[i][3]
+                for j in range(n):
+                    if foundIt:break
+                    gid2=vals[j][3]
+                    
+                    if gid1!=-1 and gid2!=-1: continue
+                    key1=k+(gid1,)
+                    key2=k+(gid2,)
+                
+            #maybe these are related.. look for neighboring dates
+                    dMin1=datetime.datetime.strptime(str(rdict[key1][minDateCol]),"%Y%m%d").date()
+                    dMax2=datetime.datetime.strptime(str(rdict[key2][maxDateCol]),"%Y%m%d").date()
+                
+                    if ( (dMin1-dMax2).days == 1 ):
+                        #make up the new entry by merging the two and add it to the dict
+                        gid=gid1
+                        key=key1
+                        if gid==-1:
+                            gid = gid2
+                            key = key2
+                        min_date=rdict[key2][minDateCol]
+                        max_date=rdict[key1][maxDateCol]
+                        maxSize1=rdict[key1][maxSizeCol]
+                        maxSize2=rdict[key2][maxSizeCol]
+                        max_size=maxSize1
+                        if maxSize2>maxSize1: max_size=maxSize2
+                        days1 = rdict[key1][daysCol]
+                        days2 = rdict[key2][daysCol]
+                        days = days1+days2
+                            
+                        ave_size = 1.0/(float(days))* (days1*float(rdict[key1][aveSizeCol]) +
+                                                       days2*float(rdict[key2][aveSizeCol]) )
+                        ave_size = int(ave_size)
+                        site=k[0]
+                        dataset=k[1]
+                        rdate=k[2]
+
+                        del rdict[key1]
+                        del rdict[key2]
+
+                        tArr = [ site, dataset,rdate,
+                                 gid, min_date, max_date,
+                                 ave_size, max_size, days ]
+
+                        rdict[key]= tArr[4:] 
+                        new3=[ tArr ]
+                        for l in range(n):
+                            if l==i: continue
+                            if l==j: continue
+                            new3.append(vals[l])
+                        rdict3[k]=new3
+                        foundIt=True
     return
 
 def process(idir, dates, fout):
@@ -93,6 +174,7 @@ def process(idir, dates, fout):
                 print('%s %s, #keys %s' % (path, time.time()-time0, len(rdict.keys())))
         sys.__stdout__.flush()
         sys.__stderr__.flush()
+    clean(rdict)    
     with open(fout, 'w') as ostream:
         ostream.write('site,dataset,rdate,gid,min_date,max_date,ave_size,max_size,days\n')
         for key, val in rdict.iteritems():
