e04464843341748ff03e42e1bdced2aa7d046bb8
mrceyhun <ceyhunuzngl@gmail.com>
2022-07-03 16:18:41 +0200

    Add yearly plots to hpc monitoring page

--- bin/cron4hpc_usage.sh
+++ bin/cron4hpc_usage.sh
@@ -6,13 +6,13 @@ set -e
 ##H    This cron job produce plots in html pages of for running cores and CoreHr monthly stats of HPC
 ##H
 ##H Usage:
-##H    cron4hpc_usage.sh <OUTPUT_DIR> <LAST_N_MONTHS> <URL_PREFIX>
+##H    cron4hpc_usage.sh <OUTPUT_DIR> <URL_PREFIX> <START_DATE> <END_DATE>
 ##H
 ##H Script arguments:
-##H    OUTPUT_DIR        Directory that output html files will be written
-##H    START_DATE        Start date of processed data
-##H    END_DATE          End date of processed data
-##H    URL_PREFIX        CernBox EOS folder url link
+##H    OUTPUT_DIR        EOS directory that output html files will be written
+##H    URL_PREFIX        CernBox EOS folder url link, i.e.: https://cmsdatapop.web.cern.ch/cmsdatapop/hpc_usage
+##H    START_DATE        Start date of processed data in YYYY-MM-DD format
+##H    END_DATE          End date of processed data YYYY-MM-DD format
 ##H
 
 # help definition
@@ -27,13 +27,31 @@ script_dir="$(
     pwd -P
 )"
 
+# Arg 1, output dir
+OUTPUT_DIR="${1}"
+# Arg 2, url prefix
+URL_PREFIX="${2}"
+# Arg 3, start date
+START_DATE="${3}"
+# Arg 4, end date
+END_DATE="${4}"
+HTML_TEMPLATE="$script_dir"/../src/html/hpc/html_template.html
+
+if [[ -z $OUTPUT_DIR || -z $URL_PREFIX || -z $START_DATE || -z $END_DATE ]]; then
+    echo '[ERROR] One or more parameters are not set'
+    echo "[ERROR] 1: ${OUTPUT_DIR} , 2: ${START_DATE} , 3: ${END_DATE} , 4: ${URL_PREFIX}"
+    exit 1
+else
+    echo "[INFO] OUTPUT_DIR: ${OUTPUT_DIR} , START_DATE: ${START_DATE} , END_DATE: ${END_DATE} , URL_PREFIX: ${URL_PREFIX}"
+fi
+
 export PYTHONPATH=$script_dir/../src/python:$PYTHONPATH
 mkdir -p "$script_dir"/../logs/cron4hpc_usage
 LOG_FILE=$script_dir/../logs/cron4hpc_usage/$(date +%Y%m%d)
 
 # Setup envs for hadoop and spark. Tested with LCG_98python3
-source /cvmfs/sft.cern.ch/lcg/views/LCG_98python3/x86_64-centos7-gcc8-opt/setup.sh
-source /cvmfs/sft.cern.ch/lcg/etc/hadoop-confext/hadoop-swan-setconf.sh analytix
+source /cvmfs/sft.cern.ch/lcg/views/LCG_101/x86_64-centos7-gcc8-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/etc/hadoop-confext/hadoop-swan-setconf.sh analytix 3.2 spark3
 
 # Check JAVA_HOME is set
 if [ -n "$JAVA_HOME" ]; then
@@ -51,16 +69,6 @@ if ! klist -s; then
     exit 1
 fi
 
-# Arg 1
-OUTPUT_DIR="${1:-/eos/user/c/cmsmonit/www/hpc_usage}"
-# Arg 2, start date
-START_DATE="${2:-2020-01-01}"
-# Arg 3, end date
-END_DATE="${3:-$(date +"%Y-%m-%d")}"
-# Arg 4
-URL_PREFIX="${4:-https://cmsdatapop.web.cern.ch/cmsdatapop/hpc_usage}"
-HTML_TEMPLATE="$script_dir"/../src/html/hpc/html_template.html
-
 echo "output directory: ${OUTPUT_DIR}"
 
 # PySpark job args
@@ -74,10 +82,10 @@ py_input_args=(
 )
 spark_submit_args=(
     --master yarn
-    --conf spark.executor.memory=4g
+    --conf spark.executor.memory=8g
     --conf spark.executor.instances=30
     --conf spark.executor.cores=4
-    --conf spark.driver.memory=4g
+    --conf spark.driver.memory=8g
     --conf spark.ui.showConsoleProgress=false
     --conf spark.sql.session.timeZone=UTC
 )
